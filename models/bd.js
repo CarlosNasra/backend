28216c4c1fc428cd291e6cede27a32de0553505c
Carlos Nasra <carlos_nasra@hotmail.com>
2022-02-20 15:51:45 -0300

    cambio timeout

--- models/bd.js
+++ models/bd.js
@@ -5,6 +5,8 @@ const { isWebAssemblyCompiledModule } = require('util/support/types');
 var pool = mysql.createPool({
     connectionlimit: 10,
     conectTimeout: 69 * 60 * 1000,
+    acquireTimeout: 60 * 60 * 1000,
+    timeout: 60 * 60 * 1000,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
